c837b976b9d386ae4ddf6886b541264490b21982
Noza <glpeznozaleda@ucsd.edu>
2025-07-08 19:54:51 -0400

    Dz -5

--- matlab/processing/Functions/create_DNS_cases.m
+++ matlab/processing/Functions/create_DNS_cases.m
@@ -29,7 +29,7 @@ function [DNS_cases, nerve_sim, lig_sim, nerve_lig_sim, zones_sim] = create_DNS_
                 DNS.continuity = "tonsils";
             end
 
-            DNS.Dz = 5:5:50; 
+            DNS.Dz = -5:5:50; 
             
             if startsWith(DNS.geom, 'b')
                 DNS.Dz = 0:1:50;
