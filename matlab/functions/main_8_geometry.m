5c5058735c4aa273a14594fe481d5a8a863c1e6b
Noza <glpeznozaleda@ucsd.edu>
2025-12-31 15:29:38 -0500

    finish-working

--- matlab/functions/main_8_geometry.m
+++ matlab/functions/main_8_geometry.m
@@ -1,19 +1,32 @@
 function main_8_geometry(cas)
 
-    fprintf("8) Geometry:\n")  
-    
-    ansys_path = fullfile(config_path('ansys', fullfile(cas.dir.chiari, 'config_file.txt')));
+    fprintf("8) Geometry:\n");
 
+    % --- ANSYS install path + version (e.g., v231) ---
+    ansys_path    = fullfile(config_path('ansys', fullfile(cas.dir.chiari, 'config_file.txt')));
     ansys_version = regexp(ansys_path, 'v\d+', 'match', 'once');
 
+    % --- SpaceClaim executable ---
     sc_path = fullfile(ansys_path, 'scdm', 'SpaceClaim.exe');
-    
-    script_clip_geometry = full_path(fullfile('..', 'ansys', 'clip_geometry_ansys.py'));
-    
-    fprintf("\tOpening SpaceClaim...\n") 
-    
-    cmd = sprintf('cmd /s /c start "" /B "%s" /RunScript="%s" "%s" "%s" "%s" > NUL 2>&1', ...
-                  sc_path, script_clip_geometry, cas.subj, full_path(cas.dir.chiari), ansys_version);
+
+    % --- SpaceClaim script to run ---
+    script_clip_geometry = full_path(fullfile('..', 'ansys', 'clip_geometry.scscript'));
+
+    % --- Write args file for SpaceClaim/Python (3 lines) ---
+    % Location: in chiari root folder
+    args_file = fullfile(pwd,  'ansys_sc_args.txt');
+
+    fid = fopen(args_file, 'w');
+    fprintf(fid, '%s\n', cas.subj);
+    fprintf(fid, '%s\n', full_path(cas.dir.chiari));
+    fprintf(fid, '%s\n', ansys_version);
+    fclose(fid);
+
+    fprintf("\tOpening SpaceClaim...\n");
+
+    % --- Launch SpaceClaim non-blocking, no console output ---
+    cmd = sprintf('cmd /s /c start "" /B "%s" /RunScript="%s" > NUL 2>&1', ...
+                  sc_path, script_clip_geometry);
 
     system(cmd);
     
